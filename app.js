e6343a97b7df972ee804fd660bc0f1d4b47189ef
Rishi700252 <kumarrishi7469@gmail.com>
2026-07-21 14:54:50 +0530

    fix: remove connect-mongo crypto to resolve Render crash

--- app.js
+++ app.js
@@ -43,17 +43,14 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const store =MongoStore.create({
     mongoUrl:dbUrl,
-    crypto:{
-       secret :process.env.SECRET,
-    },
     touchAfter: 24 * 3600,
 });
-store.on("error",() =>{
+store.on("error",(err) =>{
     console.log("ERROR in MONGO SESSION STORE",err);
 })
 const sessionOptions={
     store,
-    secret:"mysupersecretcode",
+    secret: process.env.SECRET || "mysupersecretcode",
     resave:false,
     saveUninitialized:true,
     cookie:{
